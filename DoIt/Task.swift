0ca5e62e175e917b6e4ea1c8a820450e54ee31e5
Kees van der vlis <kees@slowthinkers.net>
2017-04-17 21:05:20 +0200

    2nd screen added

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Kees van der vlis on 17/04/2017.
+//  Copyright © 2017 Kees van der vlis. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
